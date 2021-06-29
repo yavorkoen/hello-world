d4b27232bf093777cf113b97b006545c325fa691
yavorkoen <yavorkoen@gmail.com>
2021-06-29 12:56:52 +0300

    refactored solution

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,13 +1,11 @@
-for (let i = 1; i< 100; i++){
-
-    if (i % 5 == 0 && i % 3 == 0){
-        
+for (let i = 1; i <= 100; i++) {
+    if (i % 15 == 0) {
         console.log('FizzBuzz');
-    }else if( i % 5 == 0 ){
-        console.log('Buzz');
-    }else if(i % 3 == 0){
+    } else if (i % 3 == 0) {
         console.log('Fizz');
-    }else{
+    } else if (i % 5 == 0) {
+        console.log('Buzz');
+    } else {
         console.log(i);
     }
 }
